c999b5028ff62aee984a73c1b103a58bbee6955b
elkromer <hkrome@live.unc.edu>
2018-08-13 17:46:32 -0400

    first commit

---
+++ Aliases.ps1
@@ -0,0 +1,14 @@
+#Aliases
+Set-Alias -Name prg -Value cdprogramfiles
+Set-Alias -Name p86 -Value cdprogramfiles86
+Set-Alias -Name mod -Value cdmod
+Set-Alias -Name dsk -Value cddesktop
+Set-Alias -Name tlp -Value cdtoolspath
+Set-Alias -Name msc -Value cdmyscripts
+Set-Alias -Name v10 -Value cdv10
+Set-Alias -Name v19 -Value cdv19
+Set-Alias -Name z -Value Stop-Computer
+Set-Alias -Name k -Value pushkmodule
+Set-Alias -Name prf -Value openprof
+Set-Alias -Name prof -Value reloadspike
+Set-Alias -Name vm -Value cdvm
---
+++ Fields.ps1
@@ -0,0 +1,21 @@
+<#
+	.DESCRIPTION
+		Public Module fields. In order to be visible when module is loaded, variable must be added to the list in the script module file.
+	.NOTES
+		Author: Reese Krome
+		Email: reesek@cdata.com
+#>
+
+$desktop = "C:\Users\Reese Krome.DESKTOP-Q813OJU\Desktop"
+$mod = "C:\Program Files\WindowsPowerShell\Modules\SpikeHammer"
+$v10 = "C:\dev\branches\v10"
+$v19 = "C:\dev\branches\v19"
+$prg = "C:\Program Files"
+$prg86 = "C:\Program Files (x86)"
+$prof = "C:\Users\Reese Krome.DESKTOP-Q813OJU\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$vm = "C:\Users\Reese Krome.DESKTOP-Q813OJU\VirtualBox VMs"
+$myscripts = "C:\Users\Reese Krome.DESKTOP-Q813OJU\Desktop\scripts"
+$mllp = "C:\Users\Reese Krome.DESKTOP-Q813OJU\Documents\Visual Studio Projects\cs\mlppclient"
+$rudp = "C:\dev\branches\v10\IPWorksRTC\tests"
+$rudptestapp = "C:\dev\branches\v10\IPWorksRTC\tests\files\RUDP_Test_App\bin\Release"
+$nsoftwareBuildsEmailList = "reesek@cdata.com"
