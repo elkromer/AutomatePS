1a4775cfc3bd5d20461dd0a600a993af66dc085b
elkromer <hkrome@live.unc.edu>
2018-08-14 09:28:14 -0400

    removed old files

--- AutomatePS.psm1
+++ AutomatePS.psm1
@@ -4,9 +4,10 @@
 # Copyright (c) 2017 Reese Krome
 #
 
-. "$PSScriptRoot\Fields.ps1"
-. "$PSScriptRoot\Functions.ps1"
-. "$PSScriptRoot\Aliases.ps1"
-. "$PSScriptRoot\Cmdlets.ps1"
+. "$PSScriptRoot\scripts\Fields.ps1"
+. "$PSScriptRoot\scripts\Remote.ps1"
+. "$PSScriptRoot\scripts\Functions.ps1"
+. "$PSScriptRoot\scripts\Aliases.ps1"
+. "$PSScriptRoot\scripts\Cmdlets.ps1"
 
-Export-ModuleMember -Alias * -Function * -Cmdlet * -Variable desktop, mod, v10, v19, prg, prg86, myscripts, mllp, rudp, rudptestapp, nsoftwareBuildsEmailList
+Export-ModuleMember -Alias * -Function * -Cmdlet * -Variable desktop, mod, v10, v19, prg, prg86, prof, vm, myscripts, mllp, rudp, rudptestapp, nsoftwareBuildsEmailList
