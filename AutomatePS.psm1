795692931cd56ab01a6e329032548f3efba66496
elkromer <hkrome@live.unc.edu>
2019-02-04 22:38:43 -0500

    restruct and added clean script

--- AutomatePS.psm1
+++ AutomatePS.psm1
@@ -4,10 +4,10 @@
 # Copyright (c) 2017 Reese Krome
 #
 
-. "$PSScriptRoot\scripts\Fields.ps1"
-. "$PSScriptRoot\scripts\Remote.ps1"
-. "$PSScriptRoot\scripts\Functions.ps1"
-. "$PSScriptRoot\scripts\Aliases.ps1"
-. "$PSScriptRoot\scripts\Cmdlets.ps1"
+. "$PSScriptRoot\Module\Fields.ps1"
+. "$PSScriptRoot\Module\Remote.ps1"
+. "$PSScriptRoot\Module\Functions.ps1"
+. "$PSScriptRoot\Module\Aliases.ps1"
+. "$PSScriptRoot\Module\Cmdlets.ps1"
 
 Export-ModuleMember -Alias * -Function * -Cmdlet * -Variable desktop, mod, v10, v19, prg, prg86, prof, vm, myscripts, mllp, rudp, rudptestapp, nsoftwareBuildsEmailList
---
+++ scripts/clean.ps1
@@ -0,0 +1,56 @@
+#Requires -RunAsAdministrator
+param (
+
+)
+function Clean-Path {
+    param (
+    [string] $basepath,
+    [switch] $verbose
+    )
+    Push-Location $basepath
+    $sum = Get-Sum
+    Log "Removing $($sum/1000) KB from $basepath" $false $true
+    Clean
+    Pop-Location
+}
+function Get-Sum {
+    param(
+
+    )
+    $sum = 0
+    Get-ChildItem | % {
+        $sum += $_.Length
+    }
+    return $sum
+}
+function Clean {
+    param (
+
+    )
+    $sum = 0
+    Get-ChildItem | % {
+        try { Remove-Item $_ -Recurse -Force -ErrorAction Stop }
+        catch  { WriteWarning -Message "Failed to remove file." -Color "Yellow" }
+    }
+}
+function WriteWarning {
+    param (
+        [string] $warning,
+        [string] $message,
+        [string] $color
+    )
+    Log "[" $true $true
+    Log "Warning" $true $true -Color $color
+    Log "] " $true $true
+    Log $message $false $true 
+}
+
+$systemp = "C:\Windows\Temp"
+$usrtemp = "C:\Users\kouey\AppData\Local\Temp"
+
+Clean-Path $systemp
+Clean-Path $usrtemp
+
+
+
+
