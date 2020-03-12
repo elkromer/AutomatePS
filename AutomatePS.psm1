8ce723a84e30d397b376105141ac18f33116ea79
elkromer <reesek@cdata.com>
2020-03-12 11:31:51 -0400

    capitalization and cleanup

--- AutomatePS.psm1
+++ AutomatePS.psm1
@@ -6,8 +6,8 @@
 
 . "$PSScriptRoot\Lib\fields.ps1"
 . "$PSScriptRoot\Lib\functions.ps1"
-. "$PSScriptRoot\modules\cmdlets.ps1"
-. "$PSScriptRoot\modules\location.ps1"
-. "$PSScriptRoot\modules\support.ps1"
+. "$PSScriptRoot\Modules\cmdlets.ps1"
+. "$PSScriptRoot\Modules\location.ps1"
+. "$PSScriptRoot\Modules\support.ps1"
 
 Export-ModuleMember -Alias * -Function * -Variable desktop, mod, prg, prg86, prof, sysprof, profdir, emailaddress
